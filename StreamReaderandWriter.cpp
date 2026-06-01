4b27918632d09b535d0f1df67fc74b36278fdca3
sushiiyay <nfa.aliya7811@gmail.com>
2026-06-01 15:43:44 +0700

    Perulangan untuk memunculkan setiap baris

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -30,4 +30,17 @@ int main()
 
     }
     //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+    }
 }
